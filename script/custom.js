d347243154e86775181079e129221d83a5dba726
cavidann <cavi98@icloud.com>
2017-10-01 23:14:17 +0600

    not responsive

---
+++ script/custom.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+    $('.one-slide').owlCarousel({
+        items:1,        
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:4000,
+        autoplayHoverPause:true,
+        autoWidth:true,
+    })
+    $('.three-slide').owlCarousel({
+        items:3,        
+        loop:true,
+        margin:30,
+        autoplayTimeout:1000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    })
+  });
+ 
---
+++ script/init.js
@@ -0,0 +1,6 @@
+var gallery = new Slider('gallery', {
+    img_width: 100,
+    img_height: 100
+});
+
+
